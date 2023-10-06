1c116b512b250a01ced9799abc0ab5bcb89d8c82
Piyush Kumar Tiwari <piyusht823@gmail.com>
2023-10-06 18:06:18 +0530

    crossorigin added

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.employee.controller;
 
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.employee.model.Employee;
 import com.employee.service.EmployeeService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/empdata")
 public class EmployeeController {
 	
